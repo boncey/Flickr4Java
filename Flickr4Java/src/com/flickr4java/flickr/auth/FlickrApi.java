dad22da05d86fef5fb987e4b0d4e50255873062b
Allan Caplan <allan13@gmail.com>
2012-01-15 20:54:31 -0500

    Adding OAuth Stuff

---
+++ Flickr4Java/src/com/flickr4java/flickr/auth/FlickrApi.java
@@ -0,0 +1,61 @@
+/**
+ * @author acaplan
+ */
+package com.flickr4java.flickr.auth;
+
+import java.util.Scanner;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+import org.scribe.utils.OAuthEncoder;
+
+/**
+ * OAuth API for Flickr.
+ * 
+ * @author Darren Greaves
+ * @see <a href="http://www.flickr.com/services/api/">Flickr API</a>
+ */
+public class FlickrApi extends DefaultApi10a {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getAccessTokenEndpoint() {
+
+		return "http://www.flickr.com/services/oauth/access_token";
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getAuthorizationUrl(Token requestToken) {
+
+		return "http://www.flickr.com/services/oauth/authorize?oauth_token=" + requestToken.getToken();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getRequestTokenEndpoint() {
+
+		return "http://www.flickr.com/services/oauth/request_token";
+	}
+
+	/**
+	 * Get request token endpoint with a callback URL.
+	 * 
+	 * @param callbackUrl
+	 * @return
+	 */
+	public String getRequestTokenEndpoint(String callbackUrl) {
+
+		return String.format("%s?oauth_callback=%s", getRequestTokenEndpoint(), OAuthEncoder.encode(callbackUrl));
+	}
+
+}

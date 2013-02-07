59843f2b00c00294fe8c4f1f6855b52d1f878872
callmeal <allan13@gmail.com>
2013-02-06 21:50:32 -0500

    Adding files for support new Camera Interface
    http://www.flickr.com/services/api/flickr.cameras.getBrandModels.html

---
+++ Flickr4Java/src/com/flickr4java/flickr/cameras/Details.java
@@ -0,0 +1,43 @@
+package com.flickr4java.flickr.cameras;
+
+public class Details {
+    private String megapixels;
+
+    private String zoom;
+
+    private String lcdSize;
+
+    private String storageType;
+
+    public String getMegapixels() {
+        return megapixels;
+    }
+
+    public void setMegapixels(String megapixels) {
+        this.megapixels = megapixels;
+    }
+
+    public String getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(String zoom) {
+        this.zoom = zoom;
+    }
+
+    public String getLcdSize() {
+        return lcdSize;
+    }
+
+    public void setLcdSize(String lcdSize) {
+        this.lcdSize = lcdSize;
+    }
+
+    public String getStorageType() {
+        return storageType;
+    }
+
+    public void setStorageType(String storageType) {
+        this.storageType = storageType;
+    }
+}

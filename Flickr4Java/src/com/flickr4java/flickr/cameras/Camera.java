edfc7adebf52b49eacf96092378a6b0b2c767406
boncey <github@djgreaves.org>
2013-02-10 15:48:11 +0000

    Fix merge issues

--- Flickr4Java/src/com/flickr4java/flickr/cameras/Camera.java
+++ Flickr4Java/src/com/flickr4java/flickr/cameras/Camera.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 package com.flickr4java.flickr.cameras;
 
 public class Camera {
@@ -11,7 +9,7 @@ public class Camera {
     private String smallImage;
 
     private String largeImage;
-    
+
     private String name;
 
     public String getSmallImage() {
@@ -55,117 +53,3 @@ public class Camera {
     }
 
 }
-=======
-package com.flickr4java.flickr.cameras;
-
-public class Camera {
-
-    private String id;
-
-    private Details details;
-
-    private String smallImage;
-
-    private String largeImage;
-    
-    private String name;
-
-    public String getSmallImage() {
-        return smallImage;
-    }
-
-    public void setSmallImage(String smallImage) {
-        this.smallImage = smallImage;
-    }
-
-    public String getLargeImage() {
-        return largeImage;
-    }
-
-    public void setLargeImage(String largeImage) {
-        this.largeImage = largeImage;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Details getDetails() {
-        return details;
-    }
-
-    public void setDetails(Details details) {
-        this.details = details;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-}
->>>>>>> 8080af10a7f34bdce972d0e96765084e14e113f0
-=======
-package com.flickr4java.flickr.cameras;
-
-public class Camera {
-
-    private String id;
-
-    private Details details;
-
-    private String smallImage;
-
-    private String largeImage;
-    
-    private String name;
-
-    public String getSmallImage() {
-        return smallImage;
-    }
-
-    public void setSmallImage(String smallImage) {
-        this.smallImage = smallImage;
-    }
-
-    public String getLargeImage() {
-        return largeImage;
-    }
-
-    public void setLargeImage(String largeImage) {
-        this.largeImage = largeImage;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Details getDetails() {
-        return details;
-    }
-
-    public void setDetails(Details details) {
-        this.details = details;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-}
->>>>>>> 8080af10a7f34bdce972d0e96765084e14e113f0

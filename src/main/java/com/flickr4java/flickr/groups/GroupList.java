4b5d64a4cbda6927d973c0dafc5ae4dbe8c2436f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 22:17:10 +0800

    Delete src directory

--- src/main/java/com/flickr4java/flickr/groups/GroupList.java
+++
@@ -1,8 +0,0 @@
-package com.flickr4java.flickr.groups;
-
-import com.flickr4java.flickr.SearchResultList;
-
-public class GroupList<E> extends SearchResultList<Group> {
-    private static final long serialVersionUID = 3344960036515265775L;
-
-}
